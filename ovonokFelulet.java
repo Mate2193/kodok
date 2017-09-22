5c4f1b3f2f51bbaabc15f30b5b356d512093fa20
Mate2193 <21mate93@gmail.com>
2017-09-22 19:00:24 +0200

    Update ovonokFelulet.java

--- ovonokFelulet.java
+++ ovonokFelulet.java
@@ -3,5 +3,7 @@ import javax.swing.JLabel;
 public class ovonokFelulet {
     public void ovonokFelulet(){
         JLabel label3 = new JLabel();
+        
+        frame.setVisible(false);
     }
 }
