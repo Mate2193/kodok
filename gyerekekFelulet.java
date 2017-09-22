c11ec7e22c601f3d275ffe082779b55dcc19f165
Mate2193 <21mate93@gmail.com>
2017-09-22 19:00:08 +0200

    Update gyerekekFelulet.java

--- gyerekekFelulet.java
+++ gyerekekFelulet.java
@@ -3,5 +3,7 @@ import javax.swing.JLabel;
 public class gyerekekFelulet {
     public void gyerekekFelulet(){
         JLabel label2 = new JLabel();
+        
+        frame.setVisible(false);
     }
 }
